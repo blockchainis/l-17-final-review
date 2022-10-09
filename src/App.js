d8237738521bdf18fc4e839720fb82fc5145206c
hoon2 <sanghoon.engineer@gmail.com>
2022-10-09 11:33:15 +0900

    가상화폐 거래소 시작코드

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>가상화폐 거래소</h1>
+      <div>
+        <input></input>
+        <button>장바구니 담기</button>
+        <button>구매하기</button>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <input type="checkbox" />
+            <span>Bitcoin</span>
+            <button>수정</button>
+            <button>삭제</button>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <span>Ether</span>
+            <button>수정</button>
+            <button>삭제</button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
